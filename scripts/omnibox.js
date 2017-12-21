9c33f18f766a85b2ec2c4b8922f1874f2bbc22be
Andrew An <anandrew1995@gmail.com>
2017-12-21 14:32:25 -0500

    Added suggestions

--- scripts/omnibox.js
+++ scripts/omnibox.js
@@ -1,24 +1,9 @@
-chrome.omnibox.onInputChanged.addListener(
-    function(text, suggest) {
-    //   text = text.split(' ')
-    //   if (text[1] == "0") {
-    //       suggest([{content: "https://d2llabs.desire2learn.com/d2l/home", description: "Home page"}])
-    //   } else {
-    //       suggest([
-    //           {content: "https://d2llabs.desire2learn.com/d2l/home", description: "Home page"},
-    //           {content: "https://d2l.com", description: "D2L website"}
-    //       ]);
-    //   }
-});
-
-chrome.omnibox.onInputEntered.addListener(
-    function(text) {
-    //
-    // let newTabProperties = {
-    //     url: text,
-    //     active: true
-    // }
-    // chrome.tabs.create(newTabProperties)
+chrome.omnibox.onInputChanged.addListener(function (text, suggest) {
+    suggest([
+        {content: `${localStorage["quickerLinks.domain"]}/d2l/lp/configVariableBrowser`, description: "Config Variable Browser"},
+        {content: `${localStorage["quickerLinks.domain"]}/d2l/lp/manageUsers/main.d2l?ou=${localStorage["quickerLinks.domainId"]}`, description: "Users"},
+        {content: `${localStorage["quickerLinks.domain"]}/d2l/logging`, description: "System Logs"}
+    ]);
 });
 
 chrome.omnibox.onInputEntered.addListener(function (command) {
